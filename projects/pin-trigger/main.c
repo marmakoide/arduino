16125c8aad59a339360c6d365e7b6c0180c2a835
Alexandre Devert <marmakoide@hotmail.fr>
2022-03-28 21:37:40 +0200

    Fixed typos in pin-trigger/main.c

--- projects/pin-trigger/main.c
+++ projects/pin-trigger/main.c
@@ -3,32 +3,28 @@
 #include <avr/interrupt.h>
 
 
-// --- Trigger pin interrupt routine ------------------------------------------
-
 ISR(PCINT0_vect) {
 	if (bit_is_set(PINB, PINB4))
 		PORTB &= ~_BV(PORTB5); // Set pin 5 low to turn led off
 	else
-		PORTB |= _BV(PORTB5); // Set pin 5 high to turn led on
+		PORTB |= _BV(PORTB5);  // Set pin 5 high to turn led on
 }
 
 
-// --- Main entry point -------------------------------------------------------
-
 int
 main(void) {
-	// --- trigger pin setup -----------------------------------------------------
-	DDRB &= ~_BV(DDB4);    // Set pin 5 of PORTB for input
-    PORTB |= _BV(PORTB4);  // Enable pull-up
-    PCICR |= _BV(PCIE0);   // Enable "Pin Change 0" interrupt
-    PCMSK0 |= _BV(PCINT4); // PORTB4 is also PCINT4
+	// Trigger pin setup
+	DDRB &= ~_BV(DDB4);    // Set pin 4 of PORTB for input
+	PORTB |= _BV(PORTB4);  // Enable pull-up
+	PCICR |= _BV(PCIE0);   // Enable "Pin Change 0" interrupt
+	PCMSK0 |= _BV(PCINT4); // PORTB4 is also PCINT4
 
-    // --- led setup -------------------------------------------------------------
+	// Led setup
 	DDRB |= _BV(DDB5);     // Set pin 5 of PORTB for output
 	PORTB &= ~_BV(PORTB5); // Set pin 5 low to turn led off
 
-	// --- Main loop -------------------------------------------------------------
- 	sei();
+	// Main loop
+	sei();
 	while(1)
 		sleep_mode();
 }

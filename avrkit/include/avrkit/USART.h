c543fd9a3c3245ab17caa2aa3dae029a2e605f4f
Alexandre Devert <marmakoide@hotmail.fr>
2022-04-26 17:43:43 +0200

    Removed commented code section

--- avrkit/include/avrkit/USART.h
+++ avrkit/include/avrkit/USART.h
@@ -277,50 +277,6 @@ INSTANCIATE_USART_DEFINITIONS(0)
 
 #define DEFAULT_USART_RING_BUFFER_SIZE 16
 
-/*
-// Generic ring buffer defintion
-#define USART_RING_BUFFER_TYPE(STRUCT_NAME, DATA_TYPE, BUFFER_SIZE) \
-struct STRUCT_NAME { \
-    uint8_t start; \
-    uint8_t end; \
-    DATA_TYPE data[BUFFER_SIZE]; \
-}; \
-\
-inline static void \
-STRUCT_NAME ## __clear(volatile struct STRUCT_NAME * self) { \
-    self->start = self->end = 0; \
-} \
-\
-inline static bool \
-STRUCT_NAME ## __empty(volatile const struct STRUCT_NAME * self) { \
-    return self->start == self->end; \
-} \
-\
-inline static bool \
-STRUCT_NAME ## __full(volatile const struct STRUCT_NAME * self) { \
-    uint8_t next_end = (self->end + 1) % BUFFER_SIZE; \
-    return next_end == self->start; \
-} \
-\
-inline static bool \
-STRUCT_NAME ## __push(volatile struct STRUCT_NAME * self, DATA_TYPE c) { \
-    uint8_t next_end = (self->end + 1) % BUFFER_SIZE; \
-    if (next_end != self->start) { \
-	    self->data[self->end] = c; \
-	    self->end = next_end; \
-	    return 1; \
-	} \
-	return 0; \
-} \
-\
-inline static DATA_TYPE \
-STRUCT_NAME ## __pop(volatile struct STRUCT_NAME * self) { \
-    DATA_TYPE out = self->data[self->start]; \
-    self->start = (self->start + 1) % BUFFER_SIZE; \
-	return out; \
-}
-*/
-
 // Transmission ring buffer management
 #define USART_TX_BUFFER(USART_ID, STRUCT_NAME, BUFFER_SIZE) \
 STATIC_RING_BUFFER_TYPE(STRUCT_NAME, char, BUFFER_SIZE) \

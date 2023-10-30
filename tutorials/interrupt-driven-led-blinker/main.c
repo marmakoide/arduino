baae3d93fa6d0958b624ae8dc507fb67dfd50d16
Alexandre Devert <marmakoide@hotmail.fr>
2023-10-30 11:11:29 +0100

    Use the macro _BV in interrupt-drive-led-blinker example

--- tutorials/interrupt-driven-led-blinker/main.c
+++ tutorials/interrupt-driven-led-blinker/main.c
@@ -12,16 +12,16 @@ ISR(TIMER1_COMPA_vect) {
 int
 main(void) {
 	// Clear timer on compare match
-	TCCR1B |= (1 << WGM12);
+	TCCR1B |= _BV(WGM12);
 
 	// Set prescaler to 1024
-	TCCR1B |= (1 << CS12) | (1 << CS10);
+	TCCR1B |= _BV(CS12) | _BV(CS10);
 
 	// Reset timer after 15625 ticks ie. 1 sec on a 16 / 1024 Mhz clock
 	OCR1A = 15624;
 
 	// Trigger TIMER1_COMPA interruption
-	TIMSK1 |= (1 << OCIE1A);
+	TIMSK1 |= _BV(OCIE1A);
 	
 	// Set pin B5 as output
 	DDRB |= _BV(DDB5);
